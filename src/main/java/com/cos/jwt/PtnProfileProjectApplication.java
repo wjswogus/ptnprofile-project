a6b49b8143730e70cb7bf7930d3c6cdf360e7bca
wjswogus <69840575+wjswogus@users.noreply.github.com>
2020-10-26 14:57:41 +0900

    0.프로젝트 생성

---
+++ src/main/java/com/cos/jwt/PtnProfileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cos.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtnProfileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtnProfileProjectApplication.class, args);
+	}
+
+}
